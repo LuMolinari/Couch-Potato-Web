b7b6e8d7e7e19d3a0ffd423bdab6646fd1c10f39
Luca <lucamolinari1@gmail.com>
2020-12-07 19:18:24 -0800

    movie bookmarks load discover data

--- app/components/discover-tile.js
+++ app/components/discover-tile.js
@@ -2,4 +2,6 @@ import Component from "@glimmer/component";
 import ENV from "couch-potato-web/config/environment";
 import { action } from "@ember/object";
 
-export default class DiscoverTileComponent extends Component {}
+export default class DiscoverTileComponent extends Component {
+
+}
