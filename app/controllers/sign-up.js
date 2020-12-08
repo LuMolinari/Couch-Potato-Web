b8f517640e92e60c057dbc3bf41daab8cd5ec8ee
Edgar Mancia <eddiemm@users.noreply.github.com>
2020-12-07 17:34:44 -0800

    Add users into firebase auth (no checks)

--- app/controllers/sign-up.js
+++ app/controllers/sign-up.js
@@ -1,8 +1,8 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object'
+import { inject as service } from '@ember/service';
 
 
 export default class SignUpController extends Controller {
-    @action signUp(){
-    }
+    @service session; 
 }
