f3643c38c954a4216a8b4d371da96a545137b4ba
AlexisDiaz8706 <alexis.flanigan@gmail.com>
2020-12-08 18:15:49 -0800

    test2

--- app/routes/bookmarks.js
+++ app/routes/bookmarks.js
@@ -1,13 +1,47 @@
 import Route from '@ember/routing/route';
+import firebase from "firebase/app";
+import "firebase/database";
 
 export default class BookmarksRoute extends Route {
-    async model(params) {
-        //set up api
-        const response = await fetch(
-          "https://api.themoviedb.org/3/discover/movie?api_key=4517228c3cc695f9dfa1dcb4c4979152&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"        );
-        const data = await response.json();
-    
-        //return results portion of json
-        return data.results;
+
+
+
+  async model(params) {
+    var user = firebase.auth().currentUser;
+
+    //check if user is logged in
+    if (user) {
+      //first check if movie has been saved before
+      firebase
+        .database()
+        .ref("users/" + user.uid )
+        .once("value", (snapshot) => {
+          if (snapshot.exists()) {
+            //snapshot is returning the json for this particular movie saved by user id and putting it in userData
+            const userData = snapshot.val();
+            console.log("exists........", userData);
+           
+              var returnArr = [];
+          
+              snapshot.forEach(function(childSnapshot) {
+                  var item = childSnapshot.val();
+                  item.key = childSnapshot.key;
+          
+                  returnArr.push(item);
+              });
+              var myJsonString = JSON.stringify(returnArr);
+
+              console.log("test", myJsonString);
+              return myJsonString;
+          
+            
+          }
+        });
       }
-}
+    }
+
+  }
+
+
+    
+  
