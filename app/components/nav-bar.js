6912cb4407698e08420fbaf95888b4b1a55d7abc
Luca <lucamolinari1@gmail.com>
2020-12-10 15:10:19 -0800

    nav bar work

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -2,22 +2,35 @@ import Component from "@glimmer/component";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import firebase from "firebase/app";
+import "firebase/database";
 
 export default class NavBarComponent extends Component {
-  pg=1;
+  pg = 1;
   seachQuery = "";
   @service() router;
+  @service() notify;
   self = this;
 
-  @action 
-  openAccount(){
+  @action
+  openAccount() {
     var user = firebase.auth().currentUser;
 
-    if(user){
-     console.log("logged in");
-    } else{
+    if (user) {
+      console.log("logged in");
+      this.router.transitionTo("my-account");
+    } else {
+      this.router.transitionTo("sign-in");
       console.log("not logged in");
-
     }
+  }
+
+  @action
+  openDiscover() {
+    this.router.transitionTo("index", { queryParams: { page: 1 } });
+  }
+
+  @action
+  openBookmarks() {
+    this.router.transitionTo('bookmarks');
   }
 }
